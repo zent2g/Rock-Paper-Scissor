da2627cac81af3bb42852a63998cf86d0d11fe45
zent2g <155169236+zent2g@users.noreply.github.com>
2024-01-28 03:15:48 +0100

    Add playRound func with 2 parameters

--- script.js
+++ script.js
@@ -6,4 +6,21 @@ function getComputerChoice(){
 }
 //function that plays a single round of Rock Paper Scissor with 2 parameters (playerSelections and computerSelections)
 //return a string that says "You Lose! Paper beats Rock"
+function playRound(playerSelections, computerSelections){
+    if(playerSelections === computerSelections){
+        return winCon = 'It"s a tie!';
+    } else if(playerSelections === 'Rock' && computerSelections === 'Paper'){
+        return winCon = 'You lose, Paper beats Rock!'
+    } else if(playerSelections === 'Paper' && computerSelections === 'Rock'){
+        return winCon = 'You win, Paper beats Rock!'
+    } else if(playerSelections === 'Scissor' && computerSelections === 'Paper'){
+        return winCon = 'You win, Scissor beats Papper!'
+    } else if(playerSelections === 'Paper' && computerSelections === 'Scissor'){
+        return winCon = 'You Lose, Papper beats Scissor!'
+    }
+    
+}
 
+const playerSelections = 'Rock';
+const computerSelections = getComputerChoice();
+console.log(playRound(playerSelections, computerSelections));
