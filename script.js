f75ab3defa2c38e578b5b0988c1f8e9d729f73e7
zent2g <155169236+zent2g@users.noreply.github.com>
2024-01-28 02:39:09 +0100

    Add Function with Math.floor

---
+++ script.js
@@ -0,0 +1,9 @@
+//function for when computer plays
+//randomly return rock paper or scissor
+function getComputerChoice(){
+    choice = ['Rock', 'Paper', 'Scissor']
+    return choice = Math.floor(Math.random() * choice.length);
+}
+//function that plays a single round of Rock Paper Scissor with 2 parameters (playerSelections and computerSelections)
+//return a string that says "You Lose! Paper beats Rock"
+
